dfc762d846c9df0f3c18a608b787c72f6ca60d99
Arto Bendiken <arto@bendiken.net>
2025-05-09 12:47:59 +0200

    Configure the prelude & Serde integration.

--- lib/known-types/src/features.rs
+++
@@ -1,7 +0,0 @@
-// This is free and unencumbered software released into the public domain.
-
-/// The set of features that are enabled in this build of the crate.
-pub static FEATURES: &[&str] = &[
-    #[cfg(feature = "unstable")]
-    "unstable",
-];
--- lib/known-types/src/lib.rs
+++ lib/known-types/src/lib.rs
@@ -10,12 +10,12 @@
 #![deny(unsafe_code)]
 #![deny(missing_debug_implementations)]
 #![deny(clippy::unwrap_used)]
-
-pub mod c;
-
-mod features;
-pub use features::*;
+#![deny(clippy::alloc_instead_of_core)]
 
 #[cfg(doctest)]
 #[doc = include_str!("../../../README.md")]
 pub struct ReadmeDoctests;
+
+mod prelude;
+
+pub mod c;
