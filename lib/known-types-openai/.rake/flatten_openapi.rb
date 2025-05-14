79f740897020afa1a43c0c5c56cbbf9b77adad54
Arto Bendiken <arto@bendiken.net>
2025-05-14 19:07:20 +0200

    Hoist `$ref` nullability to the point of reference.

---
+++ lib/known-types-openai/.rake/flatten_openapi.rb
@@ -0,0 +1,87 @@
+# This is free and unencumbered software released into the public domain.
+
+require 'json'
+require 'yaml'
+
+module FlattenOpenAPI
+  DEFAULT_ARRAY_TYPE = { 'type': 'integer' }
+
+  ##
+  # @param  [Hash] The `openapi.components.schemas` map
+  # @return [Hash]
+  def flatten_openapi_schemas(input)
+    output = {}
+    input.each do |schema_ref, schema|
+      flatten_definition(schema_ref, schema, output)
+    end
+    output
+  end
+
+  protected
+
+  def flatten_definition(schema_ref, schema, output = {})
+    case
+      when schema['$ref']
+        output[schema_ref] = schema
+      when schema['oneOf'], schema['anyOf'], schema['allOf']
+        # TODO: allOf
+        variants = schema['oneOf'] || schema['anyOf'] || schema['allOf']
+        return flatten_definition(schema_ref, variants.first, output) if variants.size == 1
+        variants.map!.with_index do |variant, i|
+          variant_ref = [schema_ref, "Variant#{i + 1}"].join('_')
+          flatten_schema(variant_ref, variant, output)
+        end
+        output[schema_ref] = schema
+      when schema['properties'] || schema['type'] == 'object'
+        # TODO: additionalProperties
+        properties = schema['properties'] || {}
+        properties.each do |property_id, property|
+          property_ref = [schema_ref, snake_to_camel(property_id)].join('_')
+          properties[property_id] = flatten_schema(property_ref, property, output)
+        end
+        output[schema_ref] = schema
+      when schema['items'] || schema['type'] == 'array'
+        schema['items'] = flatten_schema(schema_ref, schema['items'], output)
+        output[schema_ref] = schema
+      else
+        output[schema_ref] = schema
+    end
+  end
+
+  def flatten_schema(schema_ref, schema, output = {}, level = 1)
+    case
+      when schema['$ref']
+        schema
+      when schema['oneOf'], schema['anyOf'], schema['allOf']
+        # TODO: allOf
+        variants = schema['oneOf'] || schema['anyOf'] || schema['allOf']
+        return flatten_definition(schema_ref, variants.first, output) if variants.size == 1
+        variants.map!.with_index do |variant, i|
+          variant_ref = [schema_ref, "Variant#{i + 1}"].join('_')
+          flatten_schema(variant_ref, variant, output)
+        end
+        nullable = schema.delete('nullable')
+        output[schema_ref] = schema
+        { '$ref' => "#/components/schemas/#{schema_ref}", nullable: nullable }.compact
+        # TODO: description is lost
+      when schema['properties'] || schema['type'] == 'object'
+        return schema if !schema['properties']
+        properties = schema['properties']
+        properties.each do |property_id, property|
+          property_ref = [schema_ref, snake_to_camel(property_id)].join('_')
+          properties[property_id] = flatten_schema(property_ref, property, output, level + 1)
+        end
+        output[schema_ref] = schema
+        { '$ref' => "#/components/schemas/#{schema_ref}" }
+      when schema['items'] || schema['type'] == 'array'
+        schema['items'] = schema['items'] ? flatten_schema(schema_ref, schema['items'], output) : DEFAULT_ARRAY_TYPE
+        schema
+      else
+        schema # nothing needed for primitive types
+    end
+  end
+
+  def snake_to_camel(name)
+    name.to_s.gsub(/[_.]/, ' ').split.map(&:capitalize).join
+  end
+end # FlattenOpenAPI
---
+++ lib/known-types-openai/.rake/hoist_nullables.rb
@@ -0,0 +1,41 @@
+# This is free and unencumbered software released into the public domain.
+
+module HoistNullables
+  ##
+  # @param  [Hash] The flattened `openapi.components.schemas` map
+  # @return [void]
+  def hoist_nullables!(schemas)
+    raise ArgumentError, schemas.inspect unless schemas.is_a?(Hash)
+    schemas.each_value do |schema|
+      hoist_nullable!(schema, schemas)
+    end
+  end # hoist_nullables!
+
+  ##
+  # @param  [Hash] A JSON Schema schema type
+  # @param  [Hash] The flattened `openapi.components.schemas` map
+  # @return [void]
+  def hoist_nullable!(schema, schemas)
+    raise ArgumentError, schema.inspect unless schema.is_a?(Hash)
+    raise ArgumentError, schemas.inspect unless schemas.is_a?(Hash)
+    case
+      when schema['$ref']
+        ref = schema['$ref'].split('/').last
+        #p [ref, schema['nullable'], schemas[ref]['nullable']]
+        schema['nullable'] = true if schemas[ref]['nullable']
+      when schema['oneOf'], schema['anyOf'], schema['allOf']
+        variants = schema['oneOf'] || schema['anyOf'] || schema['allOf'] || []
+        variants.each do |variant|
+          hoist_nullable!(variant, schemas)
+        end
+      when schema['properties'] || schema['type'] == 'object'
+        properties = schema['properties'] || {}
+        properties.each_value do |property|
+          hoist_nullable!(property, schemas)
+        end
+      when schema['items'] || schema['type'] == 'array'
+        hoist_nullable!(schema['items'], schemas) if schema['items']
+      else # nothing to do
+    end
+  end # hoist_nullable!
+end # HoistNullables
